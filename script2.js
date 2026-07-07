bb111e5ccd8d2a2a5c23067de66505fe8926897f
venu4340 <venuai.svpcet@gmail.com>
2026-07-07 11:40:57 +0530

    code changed

--- script2.js
+++ script2.js
@@ -1,9 +1,22 @@
-function selectElements() 
-{ 
-let titlejs = document.getElementById("title"); 
-titlejs.style.color = "blue"; 
-let messagejs = document.getElementsByClassName("message")[0]; 
-messagejs.style.color = "green"; 
-let paragraphs = document.getElementsByTagName("p"); 
-paragraphs[1].style.backgroundColor = "yellow"; 
-} 
+function selectElements() {
+  // Select element by ID
+  let titlejs = document.getElementById("title");
+  if (titlejs) {
+    titlejs.style.color = "blue";
+  }
+
+  // Select first element by class
+  let messagejs = document.getElementsByClassName("message")[0];
+  if (messagejs) {
+    messagejs.style.color = "green";
+  }
+
+  // Select second <p> element
+  let paragraphs = document.getElementsByTagName("p");
+  if (paragraphs.length > 1) {
+    paragraphs[1].style.backgroundColor = "yellow";
+  }
+}
+
+// Run automatically when the page loads
+window.onload = selectElements;
